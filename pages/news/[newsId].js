3090b843afae82a9a30a01256b76cb90889195d1
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2022-01-19 03:23:42 +0800

    Extracting Dynamic Route Data

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,12 @@
 // our-domain.com/news/{newsId}
+import { useRouter } from 'next/router'
 
 function DetailPage() {
+    const router = useRouter()
+
+    // Extract query data from URL
+    const newsId = router.query.newsId
+
     return <h1>The Detail Page</h1>
 }
 
