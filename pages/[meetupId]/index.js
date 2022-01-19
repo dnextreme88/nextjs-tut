048cac84950f194e6d5c8ff8a70b5eb26a9998d4
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2022-01-19 08:08:32 +0800

    Adding Custom Components & Styling With CSS

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,15 @@
 // our-domain.com/{meetupId}
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg'
+      title='First Meetup'
+      address='Some Street 5, Some City'
+      description='This is a First Meetup'
+    />
+  )
+}
+
+export default MeetupDetails
